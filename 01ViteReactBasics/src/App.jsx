76327bcdc8c5a6278a58892edc6036203b7353f4
sudarshan-1708 <sudarshanshuklalpu@gmail.com>
2024-02-11 01:02:13 +0530

    react + vite

---
+++ 01ViteReactBasics/src/App.jsx
@@ -0,0 +1,12 @@
+import { useState } from 'react'
+import Chai from './chai'
+function App() {
+  return (
+    <>
+    <Chai />
+    <p>Learning react with vite and chai</p>
+    </>
+  )
+}
+
+export default App
